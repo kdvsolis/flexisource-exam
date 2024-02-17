e25aacd307ffa229629e42420adc3ca0db984486
Kydo Solis <kdvsolis@gmail.com>
2024-02-17 11:50:05 +0800

    Added Exception classes

---
+++ Exceptions/RainfallNotFoundException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace flexisource_exam.Exceptions
+{
+    [Serializable]
+    internal class RainfallNotFoundException : Exception
+    {
+        public RainfallNotFoundException()
+        {
+        }
+
+        public RainfallNotFoundException(string? message) : base(message)
+        {
+        }
+
+        public RainfallNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected RainfallNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
